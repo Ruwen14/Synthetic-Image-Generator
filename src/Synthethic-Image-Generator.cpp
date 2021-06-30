c9f6d04daaaf45e89c308bd072f1a0e7e31c4a24
Ruwen14 <73891246+Ruwen14@users.noreply.github.com>
2021-07-01 00:07:07 +0200

    add

--- src/Synthethic-Image-Generator.cpp
+++ src/Synthethic-Image-Generator.cpp
@@ -31,23 +31,32 @@ void batchResize()
 }
 
 
-class Vehicle
-{
-	public:
-		virtual void doStuff() { std::cout << "Vehicle" << "\n"; }
-};
+enum class GeneratorInit { SUCCESS = 0, FAILED = 1, UNKNOWN_ERROR = 2 };
 
-class Boat : public Vehicle
+
+GeneratorInit doStuff()
 {
-public:
-	void doStuff() override { std::cout << "Boat" << "\n"; }
-};
+	return GeneratorInit::SUCCESS;
+}
 
 int main()
 {
 	simG::ImageGenerator2D::AugmentationParams params;
 
+	// Defaults
+	//params.randomnoise = false;
+	//params.BackgroundAugs.axisflipping.do_random_flip = true;
+	//params.BackgroundAugs.axisflipping.include_no_flip = true;
+	//params.BackgroundAugs.brightness.do_shift = true;
+	//params.BackgroundAugs.brightness.brightness_range = { -20.5, 20.5 };
+	//params.BackgroundAugs.crop.do_crop = true;
+	//params.BackgroundAugs.crop.target_dim = { 1024, 576 };
+	//params.BackgroundAugs.crop.keep_aspect = true;
 
+	//params.MaskAugs.rotation.do_rotate = true;
+	//params.MaskAugs.rotation.rotation_range = { 0, 360 };
+	//params.MaskAugs.brightness.do_shift = true;
+	//params.MaskAugs.brightness.brightness_range = { -20.5, 20.5 };
 
 	simG::ImageGenerator2D generator(R"(C:\Users\ruwen\Desktop\SyntheticDataGenerator_Bachelor\Dataset\input\templates\transportation\car)",
 		R"(C:\Users\ruwen\Desktop\Learning_CPP\Synthethic-Image-Generator\Test)", 500, 5, params);
@@ -56,19 +65,16 @@ int main()
 
 	simG::ImageAugmenter augmenter;
 
+
+	if (doStuff() == GeneratorInit::SUCCESS)
+	{
+		simG::print("ja");
+	}
+
 	//auto result = generator.generate();
 	simG::Directory D("./src");
 
 	auto start = std::chrono::high_resolution_clock::now();
-
-	std::vector<Vehicle> v;
-	Boat w;
-	v.push_back(w);
-
-	for (auto element: v)
-	{
-		element.doStuff();
-	}
 
 	//auto b = D.absoluteFilePath("ja");
 	
@@ -108,6 +114,10 @@ int main()
 	//	ImgaGenerator.generate();
 	//	ImgaGenerator.image_count;
 	//}
+
+
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
