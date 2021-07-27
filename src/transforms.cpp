d5d6d9ec19455af7ec24c0bcc7d7961b77941395
Ruwen14 <73891246+Ruwen14@users.noreply.github.com>
2021-07-27 22:56:47 +0200

    overhauled augmentation process with new container concept

---
+++ src/transforms.cpp
@@ -0,0 +1,330 @@
+#include "transforms.h"
+#include "Utils/Random.h"
+#include <sstream>
+
+namespace simG
+{
+	namespace transforms
+	{
+		cv::Mat RandomRotation::operator()(cv::Mat& srcSample) const
+		{
+			cv::Mat dst;
+			double degree = Random::uniformInt(rotation_range_.lower, rotation_range_.upper);
+
+			if (degree == 0)
+			{
+				return srcSample;
+			}
+			// get rotation matrix for rotating the image around its center in pixel coordinates
+			cv::Point2f image_center(static_cast<float>(srcSample.cols / 2.0), static_cast<float>(srcSample.rows / 2.0));
+			cv::Mat rotation_matrix = cv::getRotationMatrix2D(image_center, degree, 1.0);
+
+			// determine bounding rectangle
+			cv::Rect2f bbox = cv::RotatedRect(cv::Point2f(), srcSample.size(), static_cast<float>(degree)).boundingRect2f();
+
+			// adjust transformation matrix
+			rotation_matrix.at<double>(0, 2) += bbox.width / 2.0 - srcSample.cols / 2.0;
+			rotation_matrix.at<double>(1, 2) += bbox.height / 2.0 - srcSample.rows / 2.0;
+
+			// transform
+			cv::warpAffine(srcSample, dst, rotation_matrix, bbox.size());
+
+			return dst;
+		}
+
+		cv::Mat RandomRotation90::operator()(cv::Mat& srcSample) const
+		{
+			if (Random::randomProb(chance_))
+			{
+				cv::Mat dst;
+				cv::rotate(srcSample, dst, 0);
+				return dst;
+			}
+			return srcSample;
+		}
+
+		cv::Mat RandomRotation180::operator()(cv::Mat& srcSample) const
+		{
+			if (Random::randomProb(chance_))
+			{
+				cv::Mat dst;
+				cv::rotate(srcSample, dst, 1);
+				return dst;
+			}
+			return srcSample;
+		}
+		cv::Mat RandomRotation270::operator()(cv::Mat& srcSample) const
+		{
+			if (Random::randomProb(chance_))
+			{
+				cv::Mat dst;
+				cv::rotate(srcSample, dst, 2);
+				return dst;
+			}
+			return srcSample;
+		}
+
+		cv::Mat RandomHorizontalFlip::operator()(cv::Mat& srcSample) const
+		{
+			if (Random::randomProb(chance_))
+			{
+				cv::flip(srcSample, srcSample, 1);
+			}
+			return srcSample;
+		}
+
+		cv::Mat RandomVerticalFlip::operator()(cv::Mat& srcSample) const
+		{
+			if (Random::randomProb(chance_))
+			{
+				cv::flip(srcSample, srcSample, 0);
+			}
+			return srcSample;
+		}
+
+		cv::Mat RandomCrop::operator()(cv::Mat& srcSample) const
+		{
+			cv::Size img_size = srcSample.size();
+			Dimensions max_crop_dim;
+
+			if (this->keep_aspect_)
+			{
+				max_crop_dim = calcPreservedAspectRatio({ img_size.width, img_size.height }, this->crop_dim_);
+			}
+			else
+			{
+				max_crop_dim = this->crop_dim_;
+			}
+
+			assert(img_size.width >= max_crop_dim.width && img_size.height >= max_crop_dim.height);
+
+			int max_x = img_size.width - max_crop_dim.width;
+			int max_y = img_size.height - max_crop_dim.height;
+
+			int crop_x = Random::uniformInt(0, max_x);
+			int crop_y = Random::uniformInt(0, max_y);
+
+			cv::Rect roi(crop_x, crop_y, max_crop_dim.width, max_crop_dim.height);
+
+			cv::Mat cropped_img = srcSample(roi);
+			return cropped_img;
+		}
+
+		simG::transforms::Dimensions RandomCrop::calcPreservedAspectRatio(Dimensions srcDim, Dimensions targetDim) const
+		{
+			Dimensions new_dim;
+
+			if (srcDim.width == targetDim.width && srcDim.height == targetDim.height)
+			{
+				return { srcDim.width, srcDim.height };
+			}
+
+			float aspect_ratio = (float)srcDim.width / (float)srcDim.height;
+			if (targetDim.width >= targetDim.height)
+			{
+				new_dim.width = targetDim.width;
+				new_dim.height = (int)(targetDim.width / aspect_ratio);
+			}
+			else
+			{
+				new_dim.height = targetDim.height;
+				new_dim.width = (int)(targetDim.height * aspect_ratio);
+			}
+
+			return new_dim;
+		}
+
+		cv::Mat Resize::operator()(cv::Mat& srcSample) const
+		{
+			cv::Size img_size = srcSample.size();
+
+			if (img_size.width == this->dim_.width && img_size.height == this->dim_.height)
+			{
+				return srcSample;
+			}
+
+			if (this->keep_aspect_)
+			{
+				Dimensions new_dim = calcPreservedAspectRatio({ img_size.width, img_size.height }, this->dim_);
+				cv::resize(srcSample, srcSample, cv::Size(new_dim.width, new_dim.height));
+				return srcSample;
+			}
+			cv::resize(srcSample, srcSample, cv::Size(this->dim_.width, this->dim_.height));
+			return srcSample;
+		}
+
+		simG::transforms::Dimensions Resize::calcPreservedAspectRatio(Dimensions srcDim, Dimensions targetDim) const
+		{
+			Dimensions new_dim;
+
+			if (srcDim.width == targetDim.width && srcDim.height == targetDim.height)
+			{
+				return { srcDim.width, srcDim.height };
+			}
+
+			float aspect_ratio = (float)srcDim.width / (float)srcDim.height;
+			if (targetDim.width >= targetDim.height)
+			{
+				new_dim.width = targetDim.width;
+				new_dim.height = (int)(targetDim.width / aspect_ratio);
+			}
+			else
+			{
+				new_dim.height = targetDim.height;
+				new_dim.width = (int)(targetDim.height * aspect_ratio);
+			}
+
+			return new_dim;
+		}
+
+		cv::Mat RandomBrightness::operator()(cv::Mat& srcSample) const
+		{
+			double beta = Random::uniformDouble(beta_r_.lower, beta_r_.upper);
+			if (beta == 0)
+			{
+				return srcSample;
+			}
+			//std::cout << beta << "\n";
+			srcSample.convertTo(srcSample, -1, 1.0, beta);
+
+			return srcSample;
+		}
+
+		cv::Mat RandomGaussNoise::operator()(cv::Mat& srcSample) const
+		{
+			if (Random::randomProb(chance_))
+			{
+				cv::Mat mSrc_16SC;
+				cv::Mat dst;
+				cv::Mat mGaussian_noise = cv::Mat(srcSample.size(), CV_16SC3);
+				randn(mGaussian_noise, cv::Scalar::all(this->mean_), cv::Scalar::all(this->standard_dev_));
+
+				srcSample.convertTo(mSrc_16SC, CV_16SC3);
+				cv::addWeighted(mSrc_16SC, 1.0, mGaussian_noise, 1.0, 0.0, mSrc_16SC);
+				mSrc_16SC.convertTo(dst, srcSample.type());
+
+				return dst;
+			}
+			return srcSample;
+		}
+
+		cv::Mat GaussianBlur::operator()(cv::Mat& srcSample) const
+		{
+			if (Random::randomProb(chance_))
+			{
+				cv::Mat dst;
+				cv::GaussianBlur(srcSample, dst, kernelsize_, 0);
+				return dst;
+			}
+			return srcSample;
+		}
+
+		TransformsBucket::TransformsBucket(const std::vector<Transformers>& transformsList)
+		{
+			populateInternal(transformsList);
+		}
+		
+		void TransformsBucket::apply(const cv::Mat& src, cv::Mat& dst) const
+		{
+			dst = src;
+			for (const auto& transformation : transforms_)
+			{
+				dst = transformation->operator()(dst);
+			}
+		}
+
+		void TransformsBucket::operator()(const cv::Mat& src, cv::Mat& dst) const
+		{
+			apply(src, dst);
+		}
+
+		int TransformsBucket::count() const
+		{
+			return transforms_.size();
+		}
+
+		std::string TransformsBucket::dump() const
+		{
+			std::stringstream out;
+			out << typeid(*this).name() << " {\n";
+			int pos = 0;
+			for (const auto& trans : transforms_)
+			{
+				out << "\tINDEX <" << pos << ">: " << trans->getType() << "\n";
+				++pos;
+			}
+			out << "}\n";
+
+			return out.str();
+		}
+
+		void TransformsBucket::populateInternal(const std::vector<Transformers>& transforms_list)
+		{
+			for (const auto& curr_transform : transforms_list)
+			{
+				auto type_index = curr_transform.index();
+
+				switch (type_index)
+				{
+				case 0:
+					transforms_.push_back(
+						std::make_unique<RandomRotation>(std::get<RandomRotation>(curr_transform))
+					);
+					break;
+				case 1:
+					transforms_.push_back(
+						std::make_unique<RandomRotation90>(std::get<RandomRotation90>(curr_transform))
+					);
+					break;
+				case 2:
+					transforms_.push_back(
+						std::make_unique<RandomRotation180>(std::get<RandomRotation180>(curr_transform))
+					);
+					break;
+				case 3:
+					transforms_.push_back(
+						std::make_unique<RandomRotation270>(std::get<RandomRotation270>(curr_transform))
+					);
+					break;
+				case 4:
+					transforms_.push_back(
+						std::make_unique<RandomHorizontalFlip>(std::get<RandomHorizontalFlip>(curr_transform))
+					);
+					break;
+				case 5:
+					transforms_.push_back(
+						std::make_unique<RandomVerticalFlip>(std::get<RandomVerticalFlip>(curr_transform))
+					);
+					break;
+				case 6:
+					transforms_.push_back(
+						std::make_unique<RandomCrop>(std::get<RandomCrop>(curr_transform))
+					);
+					break;
+				case 7:
+					transforms_.push_back(
+						std::make_unique<Resize>(std::get<Resize>(curr_transform))
+					);
+					break;
+				case 8:
+					transforms_.push_back(
+						std::make_unique<RandomBrightness>(std::get<RandomBrightness>(curr_transform))
+					);
+					break;
+				case 9:
+					transforms_.push_back(
+						std::make_unique<RandomGaussNoise>(std::get<RandomGaussNoise>(curr_transform))
+					);
+					break;
+				case 10:
+					transforms_.push_back(
+						std::make_unique<GaussianBlur>(std::get<GaussianBlur>(curr_transform))
+					);
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ src/transforms.h
@@ -0,0 +1,271 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <variant>
+
+namespace simG
+{
+	namespace transforms
+	{
+		enum class TransformerType {
+			TYPE_TRANSFORMATION, TYPE_RANDOM_ROTATION, TYPE_RANDOM_ROTATION90, TYPE_RANDOM_ROTATION180, TYPE_RANDOM_ROTATION270,
+			TYPE_RANDOM_HORIZONTAL_FLIP, TYPE_RANDOM_VERTICAL_FLIP, TYPE_RANDOM_CROP, TYPE_RESIZE, TYPE_RANDOM_TRANSLATION,
+			TYPE_RANDOM_BRIGHTNESS, TYPE_RANDOM_GAUSS_NOISE, TYPE_GAUSS_BLUR
+		};
+
+		template<typename T>
+		struct Range
+		{
+			T lower;
+			T upper;
+		};
+
+		struct Dimensions
+		{
+			int width;
+			int height;
+		};
+
+		/** @brief Base class of all transformations. Not intended for initialization.
+		*/
+		class Transformation
+		{
+		public:
+			virtual ~Transformation() = default;
+
+			virtual cv::Mat operator() (cv::Mat& srcSample) const = 0;
+
+			std::string getType() const { return typeid(*this).name(); }
+		};
+
+		/** @brief Rotate the input by an angle selected randomly from the uniform distribution.
+		*@param range:  rotation-angle is chosen randomly between range(int min, int max).
+		*/
+		class RandomRotation : public Transformation
+		{
+		public:
+			RandomRotation(Range<int> range) : rotation_range_(range) {};
+			virtual ~RandomRotation() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		private:
+			Range<int> rotation_range_;
+		};
+
+		/** @brief Randomly rotate the input by 90 degrees.
+		*@param chance (double):  probability of applying the transform.
+		*/
+		class RandomRotation90 : public Transformation
+		{
+		public:
+			RandomRotation90(double chance) : chance_(chance) {};
+			virtual ~RandomRotation90() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		private:
+			double chance_;
+		};
+
+		/** @brief Randomly rotate the input by 180 degrees.
+		*@param chance (double):  probability of applying the transform.
+		*/
+		class RandomRotation180 : public Transformation
+		{
+		public:
+			RandomRotation180(double chance) : chance_(chance) {};
+			virtual ~RandomRotation180() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		private:
+			double chance_;
+		};
+
+		/** @brief Randomly rotate the input by 270 degrees.
+		*@param chance (double):  probability of applying the transform.
+		*/
+		class RandomRotation270 : public Transformation
+		{
+		public:
+			RandomRotation270(double chance) : chance_(chance) {};
+			virtual ~RandomRotation270() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		private:
+			double chance_;
+		};
+
+		/** @brief Randomly flip the input around y-axis.
+		*@param chance (double):  probability of applying the transform.
+		*/
+		class RandomHorizontalFlip : public Transformation
+		{
+		public:
+			RandomHorizontalFlip(double chance) : chance_(chance) {};
+			virtual ~RandomHorizontalFlip() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		private:
+			double chance_;
+		};
+
+		/** @brief Randomly flip the input around x-axis.
+		*@param chance (double):  probability of applying the transform.
+		*/
+		class RandomVerticalFlip : public Transformation
+		{
+		public:
+			RandomVerticalFlip(double chance) : chance_(chance) {};
+			virtual ~RandomVerticalFlip() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		private:
+			double chance_;
+		};
+
+		/** @brief Crop the given image at a random location. 
+		*@param targetDim (Dimensions): dimensions of the crop.
+		*@param keepAspectRatio (bool): whether to keep the aspect ratio when cropping.
+		*/
+		class RandomCrop : public Transformation
+		{
+		public:
+			RandomCrop(Dimensions targetDim, bool keepAspectRatio) :
+				crop_dim_(targetDim), keep_aspect_(keepAspectRatio) {};
+			virtual ~RandomCrop() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		private:
+			Dimensions calcPreservedAspectRatio(Dimensions srcDim, Dimensions targetDim) const;
+
+			Dimensions crop_dim_;
+			bool keep_aspect_;
+		};
+
+		/** @brief Resize the input to the given height and width.
+		*@param dim (Dimensions): desired dimensions.
+		*@param keepAspectRatio (bool): whether to keep the aspect ratio when resizing.
+		*/
+		class Resize : public Transformation
+		{
+		public:
+			Resize(Dimensions dim, bool keepAspectRatio) :
+				dim_(dim), keep_aspect_(keepAspectRatio) {};
+			virtual ~Resize() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		private:
+			Dimensions calcPreservedAspectRatio(Dimensions srcDim, Dimensions targetDim) const;
+
+			Dimensions dim_;
+			bool keep_aspect_;
+		};
+
+		class RandomTranslation : public Transformation
+		{
+		};
+
+		/** @brief Randomly change brightness of the input image.
+		*@param betaRange:  brightness factor is chosen randomly between range(double min, double max).
+		*@param alpha: alpha factor. Default = 1. 
+		*/
+		class RandomBrightness : public Transformation
+		{
+		public:
+			RandomBrightness(Range<double> betaRange, double alpha = 1)
+				: beta_r_(betaRange), alpha_(alpha) {}
+			virtual ~RandomBrightness() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		public:
+			Range<double> beta_r_;
+			double alpha_;
+		};
+
+		/** @brief Randomly apply gaussian noise to the input image.
+		*@param chance (double):  probability of applying the transform.
+		*@param mean (double): mean value.
+		*@param stdDev (double): standard deviation value.
+		*/
+		class RandomGaussNoise : public Transformation
+		{
+		public:
+			RandomGaussNoise(double chance, double mean = 0.0, double stdDev = 10.0)
+				: chance_(chance), mean_(mean), standard_dev_(stdDev) {}
+			virtual ~RandomGaussNoise() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		public:
+			double chance_;
+			double mean_;
+			double standard_dev_;
+		};
+
+		/** @brief Randomly apply gaussian blur to the input image.
+		*@param chance (double):  probability of applying the transform.
+		*@param kSize (cv::Size): kernel size.
+		*/
+		class GaussianBlur : public Transformation
+		{
+		public:
+			GaussianBlur(double chance, cv::Size kSize = { 3,3 })
+				: chance_(chance), kernelsize_(kSize) {}
+			virtual ~GaussianBlur() = default;
+
+			cv::Mat operator() (cv::Mat& srcSample) const override;
+
+		public:
+			double chance_;
+			cv::Size kernelsize_;
+		};
+
+		/** @brief A sequential container that holds transforms (see simG::transforms in transforms.h). 
+		 * Transforms are added in the order in which they are passed in the constructor.
+		 * The ``apply()`` method is used to apply selected transforms to an input sample.
+		 * @param transformsList: vector of transformers  listed in transforms.h (accessible via simG::transforms::).
+		 */
+		class TransformsBucket
+		{
+		public:
+			using Transformers = std::variant<
+				RandomRotation, RandomRotation90, RandomRotation180, RandomRotation270, RandomHorizontalFlip,
+				RandomVerticalFlip, RandomCrop, Resize, RandomBrightness, RandomGaussNoise, GaussianBlur>;
+
+			using HoldTransforms = std::vector<std::unique_ptr<Transformation>>;
+
+			TransformsBucket(const std::vector<Transformers>& transformsList);
+			TransformsBucket(const TransformsBucket&) = delete;
+			~TransformsBucket() = default;
+			/** @brief Applies selected transformations to an input sample.
+			@param src: input matrix. @param dst: destination matrix that reflects transformations.
+			*/
+			void apply(const cv::Mat& src, cv::Mat& dst) const;
+
+			/** @brief Applies selected transformations to an input sample.
+			@param src: input matrix. @param dst: destination matrix that reflects transformations.
+			*/
+			void operator()(const cv::Mat& src, cv::Mat& dst) const;
+
+			/** @brief Returns the number of transformers that the bucket currently contains.
+			*/
+			int count() const;
+
+			/** @brief Returns a printable string representation of the bucket and its contents.
+			*/
+			std::string dump() const;
+
+		public:
+			void populateInternal(const std::vector<Transformers>& transforms_list);
+
+			HoldTransforms transforms_;
+		};
+	}
+}
