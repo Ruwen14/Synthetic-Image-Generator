aca3ee1a83aaa4857a4cf18546d8339f25c7ce64
Ruwen14 <73891246+Ruwen14@users.noreply.github.com>
2022-04-23 19:49:58 +0200

    Comment out to find compile bug

--- src/simG/utils/Polygon.cpp
+++ src/simG/utils/Polygon.cpp
@@ -2,73 +2,73 @@
 
 namespace simG::geometry
 {
-	Polygon::Polygon(const PointVector& points, bool closed /*= true*/, bool simplify /*= true*/)
-	{
-		//https://stackoverflow.com/questions/23929800/is-stdmove-really-needed-on-initialization-list-of-constructor-for-heavy-membe
-		// : m_points(std::move(points))??  Only if often passed an rvalue(temporary)
-		if (simplify)
-		{
-			//simplyfy
-		}
-	}
+	//Polygon::Polygon(const PointVector& points, bool closed /*= true*/, bool simplify /*= true*/)
+	//{
+	//	//https://stackoverflow.com/questions/23929800/is-stdmove-really-needed-on-initialization-list-of-constructor-for-heavy-membe
+	//	// : m_points(std::move(points))??  Only if often passed an rvalue(temporary)
+	//	if (simplify)
+	//	{
+	//		//simplyfy
+	//	}
+	//}
 
-	Polygon::Polygon(const std::vector<cv::Point>& points, bool closed /*= true*/, bool simplify /*= true*/)
-	{
-		// : m_points(std::move(points))??
-		if (simplify)
-		{
-			//simplyfy
-		}
-	}
+	//Polygon::Polygon(const std::vector<cv::Point>& points, bool closed /*= true*/, bool simplify /*= true*/)
+	//{
+	//	// : m_points(std::move(points))??
+	//	if (simplify)
+	//	{
+	//		//simplyfy
+	//	}
+	//}
 
-	double Polygon::area()
-	{
-	}
+	//double Polygon::area()
+	//{
+	//}
 
-	size_t Polygon::size()
-	{
-		return m_points.size();
-	}
+	//size_t Polygon::size()
+	//{
+	//	return m_points.size();
+	//}
 
-	void Polygon::move(int dx, int dy)
-	{
-	}
+	//void Polygon::move(int dx, int dy)
+	//{
+	//}
 
-	void Polygon::scale(double factor)
-	{
-	}
+	//void Polygon::scale(double factor)
+	//{
+	//}
 
-	void Polygon::simplify(double tolerance)
-	{
-	}
+	//void Polygon::simplify(double tolerance)
+	//{
+	//}
 
-	bool Polygon::contains(const Polygon& poly)
-	{
-	}
+	//bool Polygon::contains(const Polygon& poly)
+	//{
+	//}
 
-	bool Polygon::contains(int px, int py)
-	{
-	}
+	//bool Polygon::contains(int px, int py)
+	//{
+	//}
 
-	bool Polygon::intersect(const Polygon& poly)
-	{
-	}
+	//bool Polygon::intersect(const Polygon& poly)
+	//{
+	//}
 
-	bool Polygon::isclosed()
-	{
-	}
+	//bool Polygon::isclosed()
+	//{
+	//}
 
-	Polygon Polygon::intersection(const Polygon& poly)
-	{
-		//https://stackoverflow.com/questions/12953127/what-are-copy-elision-and-return-value-optimization
-		//https://en.cppreference.com/w/cpp/language/move_constructor
-		//return Polygon for NRVO
-	}
+	//Polygon Polygon::intersection(const Polygon& poly)
+	//{
+	//	//https://stackoverflow.com/questions/12953127/what-are-copy-elision-and-return-value-optimization
+	//	//https://en.cppreference.com/w/cpp/language/move_constructor
+	//	//return Polygon for NRVO
+	//}
 
-	Polygon Polygon::clip(const Polygon& srcPoly, const Polygon& diffPoly)
-	{
-		//https://stackoverflow.com/questions/12953127/what-are-copy-elision-and-return-value-optimization
-		//https://en.cppreference.com/w/cpp/language/move_constructor
-		//return Polygon for NRVO
-	}
+	//Polygon Polygon::clip(const Polygon& srcPoly, const Polygon& diffPoly)
+	//{
+	//	//https://stackoverflow.com/questions/12953127/what-are-copy-elision-and-return-value-optimization
+	//	//https://en.cppreference.com/w/cpp/language/move_constructor
+	//	//return Polygon for NRVO
+	//}
 }
